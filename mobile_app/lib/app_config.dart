bca4721ff3f7202289e6a0fb850d5ab3816e30b9
Muhamad Emad Noreden Makki <Emad_341459@svuonline.org>
2026-08-08 17:20:04 +0300

    Update app_config.dart

--- mobile_app/lib/app_config.dart
+++ mobile_app/lib/app_config.dart
@@ -1,6 +1,6 @@
 class AppConfig {
   /// ضع هنا رابط Web App الخاص بـ Google Apps Script
-  static const String apiUrl = 'PASTE_APPS_SCRIPT_WEB_APP_URL_HERE';
+  static const String apiUrl = 'https://script.google.com/macros/s/AKfycbxmLi8k4o9gO8kBx-9QrcPqcnA-kC-lffnsz70YArKRU7UoqdZMUeNV37Ov-gZJaWRd/exec';
 
   static const String companyName = 'شركة أوركا ش.م.م';
   static const String companyNameEn = 'Orca L.L.C';
